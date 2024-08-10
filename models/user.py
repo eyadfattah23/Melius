b24911abde9f84a3f4ae18732e896da6584b1a32
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-08-10 22:18:23 +0300

    use default image as a profile pic for each user

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Text
+from sqlalchemy import Column, String, LargeBinary, Text
 from sqlalchemy.orm import relationship
 from hashlib import md5
 
@@ -20,10 +20,14 @@ class User(BaseModel, Base):
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     username = Column(String(128), nullable=False)
-    img = Column(Text, nullable=True)
+    img = Column(LargeBinary, nullable=True)
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+
+        if 'img' not in kwargs or not kwargs['img']:
+            with open('resources/default_male_img.jpg', 'rb') as file:
+                kwargs['img'] = file.read()
         super().__init__(*args, **kwargs)
 
     def __setattr__(self, name, value):
