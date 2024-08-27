7d1e807998e5b276097e342edc5318a10ab190f1
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-08-27 19:29:27 +0300

    handle email, password, username for a user

--- models/user.py
+++ models/user.py
@@ -8,6 +8,37 @@ import re
 from passlib.hash import bcrypt
 
 
+def password_check(passwd):
+
+    SpecialSym = ['$', '@', '#', '%']
+    val = True
+    error = None
+    if len(passwd) < 8:
+        val = False
+        error = 'length should be at least 8'
+
+    if len(passwd) > 16:
+        val = False
+        error = 'length should be not be greater than 16'
+
+    if not any(char.isdigit() for char in passwd):
+        val = False
+        error = 'Password should have at least one numeral'
+    if not any(char.isupper() for char in passwd):
+        val = False
+        error = 'Password should have at least one uppercase letter'
+
+    if not any(char.islower() for char in passwd):
+        val = False
+        error = 'Password should have at least one lowercase letter'
+
+    if not any(char in SpecialSym for char in passwd):
+        val = False
+        error = 'Password should have at least one of the symbols $%@#'
+
+    return [val, error]
+
+
 class User(BaseModel, Base):
     """Class representation of the users table
     in the database using sqlalchemy orm
@@ -19,7 +50,7 @@ class User(BaseModel, Base):
     """
 
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=False, unique=True)
     password_hash = Column(String(128), nullable=False)
     username = Column(String(128), nullable=False)
     img = Column(Text, nullable=True)   # ->changed to text (s3 aws)
@@ -46,8 +77,16 @@ class User(BaseModel, Base):
         if 'img' not in kwargs or not kwargs['img']:
             with open('resources/default_male_img.jpg', 'rb') as file:
                 kwargs['img'] = 'resources/default_male_img.jpg'
+
         if 'password' in kwargs:
+            password = kwargs.get('password')
+            val, error = password_check(password)
+            if not val:
+                raise ValueError(error)
             kwargs['password_hash'] = bcrypt.hash(kwargs.pop('password'))
+
+        if 'username' in kwargs and not kwargs['username'].strip():
+            raise ValueError("Username cannot be empty")
 
         super().__init__(*args, **kwargs)
 
@@ -56,17 +95,8 @@ class User(BaseModel, Base):
         return bcrypt.verify(password, self.password_hash)
 
     def set_password(self, password):
-        """Hash the password and set it"""
-        self.password_hash = bcrypt.hash(password)
-
-
-'''    def __setattr__(self, name, value):
-        """sets a password with md5 encryption"""
-        if name == "password":
-            value = md5(value.encode()).hexdigest()
-        super().__setattr__(name, value)
-'''
-'''
-places = relationship("Place", backref="user")
-reviews = relationship("Review", backref="user")
-'''
+        """Hash the password and set it after validation"""
+        if password_check(password):
+            self.password_hash = bcrypt.hash(password)
+        else:
+            raise ValueError("Password does not meet the required criteria.")
