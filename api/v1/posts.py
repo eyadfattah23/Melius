b7c573b84e158e92dda22248a5c71f5b7f194d87
laribox <laribox2014@gmail.com>
2024-08-30 00:58:31 +0100

    added storage to blueprint

--- api/v1/posts.py
+++ api/v1/posts.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify
 from models.post import Post
+from models import storage
 
 posts_bp = Blueprint('posts', __name__)
 
 @posts_bp.route('/posts', methods=['GET'])
 def get_posts():
-    posts = Post.query.all()
+    posts = storage.all(Post).values()
     return jsonify([post.to_dict() for post in posts])
