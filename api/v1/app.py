c16f27a439977a49f65d0a50c9b6700e86d98b69
laribox <laribox2014@gmail.com>
2024-08-30 00:47:12 +0100

    modified the app api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask
 from models.engine.db_storage_engine import db
 from os import environ
@@ -13,6 +14,41 @@ def create_flask_app():
 
     return app
 
+=======
+#!/usr/bin/python3
+from models import storage
+from flask import Flask,make_response, jsonify
+from os import environ
+from api.v1 import create_app
+
+
+def create_flask_app():
+    app = Flask(__name__)
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+    create_app(app)  # Register routes
+    @app.teardown_appcontext
+    def close_db(error):
+        """ Close Storage """
+        storage.close()
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        """ 404 Error
+        ---
+        responses:
+            404:
+            description: a resource was not found
+        """
+        return make_response(jsonify({'error': "Not found"}), 404)
+
+    return app
+
+
+
+
+>>>>>>> d4ea94c... modified the app api
 if __name__ == '__main__':
     """ Main Function """
     app = create_flask_app()
@@ -22,4 +58,8 @@ if __name__ == '__main__':
         host = '0.0.0.0'
     if not port:
         port = '5000'
+<<<<<<< HEAD
     app.run(host=host, port=port, threaded=True)
+=======
+    app.run(host=host, port=port, threaded=True)
+>>>>>>> d4ea94c... modified the app api
