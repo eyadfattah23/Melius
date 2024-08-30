6e8358b960a51cade88edd77550de8ef5d25d927
laribox <laribox2014@gmail.com>
2024-08-30 01:38:47 +0100

    added all entry points for the users api calls

--- api/v1/users.py
+++ api/v1/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, make_response, abort
 from models.user import User
 from models import storage
 
@@ -8,3 +8,72 @@ users_bp = Blueprint('users', __name__)
 def get_users():
     users = storage.all(User).values()
     return jsonify([user.to_dict() for user in users])
+
+# Creates a new user
+@users_bp.route('/api/users', methods=['POST'])
+def create_user():
+    """
+    Creates a user
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'email' not in request.get_json():
+        abort(400, description="Missing email")
+    if 'password' not in request.get_json():
+        abort(400, description="Missing password")
+
+    data = request.get_json()
+    instance = User(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+# Retrieves specific user details
+@users_bp.route('/api/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    """ Retrieves an user """
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+
+    return jsonify(user.to_dict())
+
+# Updates user info
+@users_bp.route('/api/users/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    """
+    Updates a user
+    """
+    user = storage.get(User, user_id)
+
+    if not user:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'email', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(user, key, value)
+    storage.save()
+    return make_response(jsonify(user.to_dict()), 200)
+
+# Deletes a user and all associated data
+@users_bp.route('/api/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """
+    Deletes a user Object
+    """
+
+    user = storage.get(User, user_id)
+
+    if not user:
+        abort(404)
+
+    storage.delete(user)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
