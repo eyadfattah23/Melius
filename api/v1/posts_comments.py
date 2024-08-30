f2fb8757dc22d1d373dbe4a92244b33be298a763
laribox <laribox2014@gmail.com>
2024-08-30 04:39:47 +0100

    created the entry points API for post comments

---
+++ api/v1/posts_comments.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, jsonify, request, make_response, abort
+from models.post import Post, PostComment
+from models import storage
+
+comments_bp = Blueprint('posts', __name__)
+
+
+# Retrieves all comments for a specific post
+@comments_bp.route('/posts/<post_id>/comments', methods=['GET'])
+def get_post_comments(post_id):
+    post =  storage.get(Post, post_id)
+    return jsonify([comment.to_dict() for comment in post.comments])
+
+
+# Creates a new comment for a specific post
+@comments_bp.route('/posts/<post_id>/comments', methods=['POST'])
+def create_post_comment(post_id):
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'text' not in request.get_json():
+        abort(400, description="Missing text")
+
+    data = request.get_json()
+    instance = PostComment(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+
+# Updates a comment for a specific post
+@comments_bp.route('/posts/<post_id>/comments/<comment_id>', methods=['PUT'])
+def update_post_comment(post_id, comment_id):
+    postComment =  storage.get(PostComment, comment_id)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'user_id', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(postComment, key, value)
+    storage.save()
+    return make_response(jsonify(postComment.to_dict()), 200)
