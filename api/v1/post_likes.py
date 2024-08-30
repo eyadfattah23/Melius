74253169bc57b3ecc50450ea13600f26675300b2
laribox <laribox2014@gmail.com>
2024-08-30 14:31:31 +0100

    end point for the post likes

--- api/v1/post_likes.py
+++ api/v1/post_likes.py
@@ -12,3 +12,40 @@ def get_post_likes(post_id):
   return jsonify([like.to_dict() for like in post.likes])
 
 
+# Creates a new like
+@likes_bp.route('/posts/<post_id>/likes', methods=['POST'])
+def create_post_like(post_id):
+  post =  storage.get(Post, post_id)
+
+  if not post:
+    abort(404)
+
+  if not request.get_json():
+    abort(400, description="Not a JSON")
+
+  data = request.get_json()
+
+  instance = PostLike(**data)
+  instance.post = post
+  instance.save()
+  return make_response(jsonify(instance.to_dict()), 201)
+
+
+# Deletes a like
+@likes_bp.route('/posts/<post_id>/likes/<like_id>', methods=['DELETE'])
+def delete_post_like(post_id, like_id):
+  post =  storage.get(Post, post_id)
+
+  if not post:
+    abort(404)
+
+  like =  storage.get(PostLike, like_id)
+
+  if not like:
+    abort(404)
+
+  storage.delete(like)
+  storage.save()
+  return make_response(jsonify({"message": "Like deleted"}), 200)
+
+
