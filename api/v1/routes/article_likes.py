7ba314bead545b49dbe18ed7f4e1da29da6f7e80
laribox <laribox2014@gmail.com>
2024-08-31 15:31:16 +0100

    docs for post

--- api/v1/routes/article_likes.py
+++ api/v1/routes/article_likes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, abort, request, make_response
 from models.article import ArticleLike, Article
 from models import storage
+from flasgger.utils import swag_from
 
 
 article_likes_bp = Blueprint('articleLikes', __name__)
