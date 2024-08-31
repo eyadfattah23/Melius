c6bfbb8df96b5d6b1f8fefb95cb867aae36a0d05
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-08-31 13:48:43 +0100

    implement community, articles, and challenge pages

---
+++ frontend/src/pages/articles.jsx
@@ -0,0 +1,26 @@
+import Navbar from "../components/navbar"
+import Articles_Card from "../components/articles_card"
+import "../assets/styles/articles.css"
+import Articles_list from "../components/articles_tab"
+function Articles(){
+    return <>
+    <Navbar/>
+    <section className="main_article_section">
+        <div className="container">
+        <h1> Article of The Week</h1>
+        <div className="main_article_card">
+           <Articles_Card orientation={"horizontal"}/>
+        </div>
+        </div>
+    </section>
+    
+    <section className="articles_section">
+                <Articles_list/>
+                    </section>
+    </>
+}
+export default Articles
+
+
+
+
