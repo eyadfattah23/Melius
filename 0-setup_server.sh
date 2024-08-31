4826c4938d90ac8d4f201b5cf9bb076e165e4abc
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-09-01 00:00:37 +0300

    gunicorn, tcp and venv installment and running

--- 0-setup_server.sh
+++ 0-setup_server.sh
@@ -18,8 +18,21 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
 sudo ufw allow https
+sudo ufw allow tcp
 sudo ufw allow http
+sudo ufw allow 5050
+sudo ufw allow 5000
+
+
 
 echo 'y'| sudo ufw enable
 
-pip3 install -r requirements.txt
+
+sudo apt -y install python3-venv
+
+python -m venv new-venv
+
+# source venv/bin/activate
+# pip3 install -r requirements.txt
+
+pip3 install gunicorn
