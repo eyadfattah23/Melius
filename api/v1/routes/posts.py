65908c89f887c85cce18fa1f2a56171b0441d62e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-09-03 19:07:07 +0300

    update for get /posts 1. add comments and likes count 2. handle pagination using the awesome new method of larini 3. add a url parameter to see if the user liked a post of not

--- api/v1/routes/posts.py
+++ api/v1/routes/posts.py
@@ -1,17 +1,60 @@
 from flask import Blueprint, jsonify, request, make_response, abort
-from models.post import Post
+from models.post import *
 from models import storage
 from flasgger.utils import swag_from
 
 posts_bp = Blueprint('posts', __name__)
 
+
 @swag_from('documentation/post/get_posts.yml')
 @posts_bp.route('/posts', methods=['GET'])
 def get_posts():
-    posts = storage.all(Post).values()
-    return jsonify([post.to_dict() for post in posts])
+    """route to get all posts paginated"""
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 10, type=int)
+    user_id = request.args.get('user_id', '', type=str)
+
+    posts = list(storage.all(Post, page=page, page_size=per_page).values())
+
+    total_posts = storage.count(Post)
+    # Paginate the posts
+    start = (page - 1) * per_page
+    end = start + per_page
+    paginated_posts = posts[start:end]
+
+    # Prepare the paginated response
+    posts_list = []
+    for post in paginated_posts:
+        post_dict = post.to_dict().copy()
+
+        likes_count = storage.count(PostLike, post_id=post.id)
+        comments_count = storage.count(PostComment, post_id=post.id)
+
+        post_dict['likes_count'] = likes_count
+        post_dict['comments_count'] = comments_count
+
+        if user_id:
+            like = storage.getSession().query(PostLike).filter_by(
+                post_id=post.id, user_id=user_id).first()
+            if like:
+                post_dict['liked'] = True
+            else:
+                post_dict['liked'] = False
+
+        posts_list.append(post_dict)
+
+    response = {
+        'total_posts': total_posts,
+        'page': page,
+        'per_page': per_page,
+        'total_pages': (total_posts + per_page - 1) // per_page,
+        'posts': posts_list
+    }
+    return jsonify(response)
 
 # Creates a new post
+
+
 @swag_from('documentation/post/create_post.yml')
 @posts_bp.route('/posts', methods=['POST'])
 def create_post():
@@ -34,6 +77,8 @@ def create_post():
     return make_response(jsonify(instance.to_dict()), 201)
 
 # Retrieves specific post info
+
+
 @swag_from('documentation/post/get_post.yml')
 @posts_bp.route('/posts/<post_id>', methods=['GET'])
 def get_post(post_id):
@@ -45,6 +90,8 @@ def get_post(post_id):
     return jsonify(post.to_dict())
 
 # Updates post content
+
+
 @swag_from('documentation/post/update_post.yml')
 @posts_bp.route('/posts/<post_id>', methods=['PUT'])
 def update_post(post_id):
@@ -70,9 +117,11 @@ def update_post(post_id):
     return make_response(jsonify(post.to_dict()), 200)
 
 # Deletes a post and all associated data
+
+
 @swag_from('documentation/post/delete_post.yml')
 @posts_bp.route('/posts/<post_id>', methods=['DELETE'])
-def delete_post(post_id):    
+def delete_post(post_id):
     ''' Deletes a post and all associated data '''
     post = storage.get(Post, post_id)
 
@@ -88,5 +137,5 @@ def delete_post(post_id):
 # Retrieves all comments for a specific post
 @posts_bp.route('/posts/<post_id>/comments', methods=['GET'])
 def get_post_comments(post_id):
-    post =  storage.get(Post, post_id)
+    post = storage.get(Post, post_id)
     return jsonify([comment.to_dict() for comment in post.comments])
