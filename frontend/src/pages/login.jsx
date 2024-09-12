447e5079d6d610cee4327ac54122e2f67ff7845d
larini abdellah <laribox2014@gmail.com>
2024-09-12 05:32:49 +0100

    fix the login field color to white

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,120 +1,147 @@
-import Button from "../components/common/button"
-import Navbar from "../components/common/navbar"
-import "../assets/styles/login.css"
-import Icon from "../assets/icons/icon"
-import Field from "../components/common/field"
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
-import countNumberOfDays from "../functions/count_number_of_days"
-import countLevel from "../functions/count_level"
-function Login(){
-    const [email, setEmail] = useState("")
-    const [loading, setLoading] = useState(false);
-    const [password, setPassword] = useState("");
-    const [errorEmail, setErrorEmail] = useState("");
-    const [errorPassword, setErrorPassword] = useState("");
-    const navigate = useNavigate()
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setLoading(true);
-        setErrorEmail("");
-        setErrorPassword("");
+import Button from "../components/common/button";
+import Navbar from "../components/common/navbar";
+import "../assets/styles/login.css";
+import Icon from "../assets/icons/icon";
+import Field from "../components/common/field";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import countNumberOfDays from "../functions/count_number_of_days";
+import countLevel from "../functions/count_level";
+function Login() {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setErrorEmail("");
+    setErrorPassword("");
 
-        let hasError = false;
+    let hasError = false;
 
-        if (!email) {
-            setErrorEmail("Email is required");
-            hasError = true;
-        } else {
-            const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-            if (!emailRegex.test(email)) {
-                setErrorEmail("Invalid email format");
-                hasError = true;
-            }
+    if (!email) {
+      setErrorEmail("Email is required");
+      hasError = true;
+    } else {
+      const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+      if (!emailRegex.test(email)) {
+        setErrorEmail("Invalid email format");
+        hasError = true;
+      }
+    }
+    if (!password) {
+      setErrorPassword("Password is required");
+      hasError = true;
+    }
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:5050/api/v1/users/authenticate",
+        {
+          email,
+          password,
         }
-        if (!password) {
-            setErrorPassword("Password is required");
-            hasError = true;
-        }
-        try {
-            const response = await axios.post("http://127.0.0.1:5050/api/v1/users/authenticate", {
-                email,
-                password,
-            });
+      );
 
-            console.log(response);
-            const img = response.data.user.img
-            localStorage.setItem("token",  JSON.stringify(response.data.token))
-            localStorage.setItem("username", JSON.stringify(response.data.user.username));
-            localStorage.setItem("user_id", JSON.stringify(response.data.user.id));
-            console.log(response.data.user.timer_reset_date)
-            localStorage.setItem("number_of_days", JSON.stringify(countNumberOfDays(response.data.user.timer_reset_date)));
-            localStorage.setItem("level", JSON.stringify(countLevel(countNumberOfDays(response.data.user.timer_reset_date))));
-            navigate("/home");
-        } catch (error) {
-            console.error(error);
-            setErrorPassword(error.response?.data?.error || "An error occurred. Please try again.");
-        } finally {
-            setLoading(false);
-        }
-    };
-    return<>
-    <Navbar/>
-    <section className="login_section">
+      console.log(response);
+      const img = response.data.user.img;
+      localStorage.setItem("token", JSON.stringify(response.data.token));
+      localStorage.setItem(
+        "username",
+        JSON.stringify(response.data.user.username)
+      );
+      localStorage.setItem("user_id", JSON.stringify(response.data.user.id));
+      console.log(response.data.user.timer_reset_date);
+      localStorage.setItem(
+        "number_of_days",
+        JSON.stringify(countNumberOfDays(response.data.user.timer_reset_date))
+      );
+      localStorage.setItem(
+        "level",
+        JSON.stringify(
+          countLevel(countNumberOfDays(response.data.user.timer_reset_date))
+        )
+      );
+      navigate("/home");
+    } catch (error) {
+      console.error(error);
+      setErrorPassword(
+        error.response?.data?.error || "An error occurred. Please try again."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <Navbar />
+      <section className="login_section">
         <div className="left_container">
-            <div className="user">
-                <Icon name={"user"} size={96} color={"white"}/>
-            </div>
-            <div className="login">
-                <div className="form">
-                    <div className="field">
-                       <div className="container">
-                       <Icon name={"email_fill"} size={24} color={"white"}/> 
-                       <Field 
-                        placeholder="Email"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                       />
-                       </div>
-                    {errorEmail && <p className="error">{errorEmail}</p>}
-                    </div>
-                    <div className="field">
-                        <div className="container">
-                        <Icon name={"lock"} size={24} color={"white"}/> 
-                    <Field 
+          <div className="user">
+            <Icon name={"user"} size={96} color={"white"} />
+          </div>
+          <div className="login">
+            <div className="form">
+              <div className="field">
+                <div className="container">
+                  <Icon name={"email_fill"} size={24} color={"white"} />
+                  <Field
+                    placeholder="Email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    color="white"
+                  />
+                </div>
+                {errorEmail && <p className="error">{errorEmail}</p>}
+              </div>
+              <div className="field">
+                <div className="container">
+                  <Icon name={"lock"} size={24} color={"white"} />
+                  <Field
                     placeholder="********"
                     type="password"
                     value={password}
                     required
-                    onChange={(e) => setPassword(e.target.value)}/>
-                        </div>
-                    {errorPassword && <p className="error">{errorPassword}</p>}
-                    </div>
-
-                    <div className="submit">
-                        <Button text={loading ? "Loging In..." : "LOGIN"} type={"login_btn"} onClick={handleSubmit}/>
-                    </div>
+                    color="white"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
+                {errorPassword && <p className="error">{errorPassword}</p>}
+              </div>
+
+              <div className="submit">
+                <Button
+                  text={loading ? "Loging In..." : "LOGIN"}
+                  type={"login_btn"}
+                  onClick={handleSubmit}
+                />
+              </div>
             </div>
-            <div className="alternative">
+          </div>
+          <div className="alternative">
             <p>New here ?</p>
             <div>
-                <Link to={"/signup"}>
-                <Button text={"Create an account"} type={"signup_btn_link"}/>
-                </Link>
+              <Link to={"/signup"}>
+                <Button text={"Create an account"} type={"signup_btn_link"} />
+              </Link>
             </div>
+          </div>
         </div>
-        </div>
-       
+
         <div className="right_container">
-            <p className="title">Welcome Back to Your Path</p>
-            <p className="description">Log in to continue your healing journey and stay on track</p>
+          <p className="title">Welcome Back to Your Path</p>
+          <p className="description">
+            Log in to continue your healing journey and stay on track
+          </p>
         </div>
-    </section>
+      </section>
     </>
+  );
 }
-export default Login
+export default Login;
