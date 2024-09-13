020172884b4ff535a16b10b04da6a1f86ed76dbf
larini abdellah <laribox2014@gmail.com>
2024-09-13 16:22:59 +0100

    Add a global config for the front end to specify the api url

--- frontend/src/components/common/edit_post.jsx
+++ frontend/src/components/common/edit_post.jsx
@@ -1,87 +1,99 @@
-import Button from "../common/button"
-import { useState } from "react"
-import axios from "axios"
-import "../../assets/styles/common/edit_profile.css"
-import { DialogContent, DialogFooter, DialogHeader, DialogTitle} from "../../components/shadcn/ui/dialog"
-import Field from "../common/field"
-export default function EditPost({post_id,title, text}) {
-    const token = JSON.parse(localStorage.getItem("token"))
-    const [postTitle, setTitle] = useState(title)
-    const [ErrorTitle, setErrorTitle] = useState("")
-    const [postText, setText] = useState(text)
-    const [ErrorText, setErrorText] = useState("")
-    const [loading, setLoading]= useState(false)
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setLoading(true);
-        setErrorTitle("");
-        setErrorText("");
+import Button from "../common/button";
+import { useState } from "react";
+import axios from "axios";
+import "../../assets/styles/common/edit_profile.css";
+import {
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../../components/shadcn/ui/dialog";
+import Field from "../common/field";
+import config from "@/config";
+export default function EditPost({ post_id, title, text }) {
+  const token = JSON.parse(localStorage.getItem("token"));
+  const [postTitle, setTitle] = useState(title);
+  const [ErrorTitle, setErrorTitle] = useState("");
+  const [postText, setText] = useState(text);
+  const [ErrorText, setErrorText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setErrorTitle("");
+    setErrorText("");
 
-        let hasError = false;
-        if (hasError) {
-            setLoading(false);
-            return;
+    let hasError = false;
+    if (hasError) {
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await axios.put(
+        config.API_URL + "posts/" + post_id,
+        {
+          title: postTitle,
+          text: postText,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
+      );
 
-        try {
-            const response = await axios.put("http://127.0.0.1:5050/api/v1/posts/" + post_id, {
-                title: postTitle,
-                text: postText,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                  },
-            }
-        );
-
-            console.log(response);
-            window.location.reload()
-        } catch (error) {
-            console.error(error);
-            setErrorText(error.response?.data?.error || "An error occurred. Please try again.");
-        } finally {
-            setLoading(false);
-        }
-    };
+      console.log(response);
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+      setErrorText(
+        error.response?.data?.error || "An error occurred. Please try again."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-      <DialogContent className="sm:max-w-[726px] create_post_main">
-       <DialogHeader >
-    <DialogTitle className={"title"}>New Post</DialogTitle>
-    </DialogHeader>
-    <div className="grid gap-4 py-4">
-                        <div className="form">
-                            <div className="field">
-                                <div className="container title_field">
-                                    <Field
-                                        placeholder="Enter a title"
-                                        type="text"
-                                        value={postTitle}
-                                        onChange={(e) => setTitle(e.target.value)}
-                                        required
-                                    />
-                                </div>
-                                {ErrorTitle && <p className="error">{ErrorTitle}</p>}
-                            </div>
-                            <div className="field">
-                                <div className="container content_field">
-                                    <Field
-                                        placeholder="Enter your content"
-                                        type="text"
-                                        value={postText}
-                                        required
-                                        onChange={(e) => setText(e.target.value)}
-                                    />
-                                </div>
-                                {ErrorText && <p className="error">{ErrorText}</p>}
-                            </div>
-                        </div>
-    </div>
-    <DialogFooter>
-    <Button text={loading ? "Loading..." : "Post"} type="cta_filled" onClick={handleSubmit} />
-    </DialogFooter> 
-     </DialogContent>
-   
-  )
+    <DialogContent className="sm:max-w-[726px] create_post_main">
+      <DialogHeader>
+        <DialogTitle className={"title"}>New Post</DialogTitle>
+      </DialogHeader>
+      <div className="grid gap-4 py-4">
+        <div className="form">
+          <div className="field">
+            <div className="container title_field">
+              <Field
+                placeholder="Enter a title"
+                type="text"
+                value={postTitle}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </div>
+            {ErrorTitle && <p className="error">{ErrorTitle}</p>}
+          </div>
+          <div className="field">
+            <div className="container content_field">
+              <Field
+                placeholder="Enter your content"
+                type="text"
+                value={postText}
+                required
+                onChange={(e) => setText(e.target.value)}
+              />
+            </div>
+            {ErrorText && <p className="error">{ErrorText}</p>}
+          </div>
+        </div>
+      </div>
+      <DialogFooter>
+        <Button
+          text={loading ? "Loading..." : "Post"}
+          type="cta_filled"
+          onClick={handleSubmit}
+        />
+      </DialogFooter>
+    </DialogContent>
+  );
 }
-
--- frontend/src/components/common/post_options.jsx
+++ frontend/src/components/common/post_options.jsx
@@ -1,44 +1,57 @@
-import Icon from "../../assets/icons/icon"
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem } from "../shadcn/ui/dropdown-menu"
-import { Dialog, DialogTrigger } from "../shadcn/ui/dialog"
-import axios from "axios"
-import EditPost from "./edit_post"
-const PostOptions =({post_id, title, text}) =>{
-    const token = JSON.parse(localStorage.getItem("token"))
-    const handleDelete =async() =>{
-        try {
-            const response = await axios.delete(`http://127.0.0.1:5050/api/v1/posts/${post_id}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                  },
-            }) 
-            window.location.reload()         
-        } catch (error) {
-            console.error(error);
-        } finally {
-            // setLoading(false);
-        }
-      }
-    return  <DropdownMenu > <DropdownMenuTrigger asChild>
-    <button><Icon name={"more"} size={24}/></button>
-</DropdownMenuTrigger>
-<DropdownMenuContent className="w-56 post_options_dropdown">
-<DropdownMenuGroup>
-<Dialog>
-  <DialogTrigger asChild>
-  <div className={"menu_item"}>
-  <Icon name={"pencil"} size={16} />
- <span>Edit Post</span>
- </div>
-  </DialogTrigger>
-  <EditPost title = {title} text={text} post_id={post_id}/>
-</Dialog> 
-<DropdownMenuItem className={"menu_item"} onClick={handleDelete}>
-<Icon name={"delete"} size={16}/>
-<span>Delete Post</span>
-</DropdownMenuItem>
-</DropdownMenuGroup>
-</DropdownMenuContent>
-</DropdownMenu>
-}
-export default PostOptions
+import Icon from "../../assets/icons/icon";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+} from "../shadcn/ui/dropdown-menu";
+import { Dialog, DialogTrigger } from "../shadcn/ui/dialog";
+import axios from "axios";
+import EditPost from "./edit_post";
+import config from "@/config";
+const PostOptions = ({ post_id, title, text }) => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(config.API_URL + `posts/${post_id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // setLoading(false);
+    }
+  };
+  return (
+    <DropdownMenu>
+      {" "}
+      <DropdownMenuTrigger asChild>
+        <button>
+          <Icon name={"more"} size={24} />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 post_options_dropdown">
+        <DropdownMenuGroup>
+          <Dialog>
+            <DialogTrigger asChild>
+              <div className={"menu_item"}>
+                <Icon name={"pencil"} size={16} />
+                <span>Edit Post</span>
+              </div>
+            </DialogTrigger>
+            <EditPost title={title} text={text} post_id={post_id} />
+          </Dialog>
+          <DropdownMenuItem className={"menu_item"} onClick={handleDelete}>
+            <Icon name={"delete"} size={16} />
+            <span>Delete Post</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+export default PostOptions;
