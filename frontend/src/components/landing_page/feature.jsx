347d960b5175f9e21c9425e55ee8646b7a306d02
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-09-18 23:18:37 +0100

    add features pic

--- frontend/src/components/landing_page/feature.jsx
+++ frontend/src/components/landing_page/feature.jsx
@@ -1,16 +1,48 @@
-function Feature({icon, title, description}){
-    return <div className="feature_item flex flex-col justify-center items-center gap-4 ">
-        <div className="content flex flex-col items-center gap-4 ">
-            <div className="header flex items-center justify-center gap-2">
-                {icon}
+
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+import './style.css';
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
+function Feature({icon, title, description, image1, image2, reverse}){
+    return <div  className={`flex flex-col items-center gap-8  lg:px-8 ${reverse ? "lg:flex-row-reverse" : "lg:flex-row"}`} >
+        
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <img src={image1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={image2} alt="" />
+            </SwiperSlide>
+      
+        
+      </Swiper>
+    
+        <div className={`content flex flex-col justify-between gap-8 lg:p-0 ${reverse && "py-8"}`}>
                 <h4>
                     {title}
                 </h4>
-            </div>
             <p>
                 {description}
             </p>
         </div>
+       
     </div>
 }
 export default Feature
+
+
+
--- frontend/src/components/landing_page/features.jsx
+++ frontend/src/components/landing_page/features.jsx
@@ -1,23 +1,33 @@
 import Icon from "../../assets/icons/icon"
 import Feature from "./feature"
-
+import Tracking1 from "../../assets/images/features/tracking1.png"
+import Tracking2 from "../../assets/images/features/tracking2.png"
+import Community1 from "../../assets/images/features/community2.png"
+import Community2 from "../../assets/images/features/community1.png"
+import Articles1 from "../../assets/images/features/articles.png"
+import Articles2 from "../../assets/images/features/articles2.png"
 function Features() {
-    return <section className="full_screen_section flex flex-col  items-center justify-center px-16 gap-8  landing_features" id="features">
+    return <section className="landing_features flex flex-col  items-center justify-center px-16 gap-8" id="features">
         <h1>Explore Our Features</h1>
-        <div className="features_list flex flex-col lg:flex-row items-center justify-center lg:space-x-12 space-y-8 lg:space-y-0">
+        <div className="lg:px-16 gap-16 flex flex-col items-center features_list">
             <Feature
-                icon={<Icon name={"calendar_todo"} size={24} color={"icon_secondary"} />}
                 title={"Progress Tracker"}
                 description={
                     "Track your no-porn days with our personalized timer. As you progress, earn badges that celebrate your milestones and encourage you to keep going."
                 }
+                image1={Tracking1}
+                image2 = {Tracking2}
             />
             <Feature
-                icon={<Icon name={"group_users"} 
-                size={24} color={"icon_secondary"} />} 
+                image1={Community1}
+                image2 = {Community2}
                 title={"Community"} 
+                reverse
                 description={"Connect with others who are going through similar experiences. Share your progress, read others’ stories, and find support through the Melius community."} />
-            <Feature icon={<Icon name={"book_open"} size={24} color={"icon_secondary"} />} title={"Educational Content"} description={"Access expert content that explore porn addiction from different perspectives—mental health, religion, relationships, and personal growth."} />
+            <Feature  
+             image1={Articles1}
+             image2 = {Articles2}
+            title={"Educational Content"} description={"Access expert content that explore porn addiction from different perspectives—mental health, religion, relationships, and personal growth."} />
 
         </div>
     </section>
