7b007ee3a304262bb3400ae8b49a3778db800416
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-09-19 02:13:31 +0100

    style landing page

--- frontend/src/components/landing_page/about.jsx
+++ frontend/src/components/landing_page/about.jsx
@@ -6,17 +6,16 @@ function About(){
     return <div className="landing_about flex flex-col justify-center items-center gap-8 px-16 " id="about">
        <h1>Why Melius</h1>
        <div className="gap-8 flex flex-col">
-        <p>The idea behind Melius started from a personal experience with a close friend struggling with porn addiction. Watching this friend battle the addiction and trying different solutions without success motivated the team to create something that addresses the root of the problem in a more effective and compassionate way. We believe that privacy, community support, and easily accessible tools are crucial to overcoming this challenge. With this passion, the Melius team is dedicated to helping others take control of their lives.
+        <p className="text-center">The idea behind Melius started from a personal experience with a close friend struggling with porn addiction. Watching this friend battle the addiction and trying different solutions without success motivated the team to create something that addresses the root of the problem in a more effective and compassionate way. We believe that privacy, community support, and easily accessible tools are crucial to overcoming this challenge. With this passion, the Melius team is dedicated to helping others take control of their lives.
 
 Melius has grown into more than just a technical project. It’s a tool built with empathy, a product of deep thought and a desire to make a real difference in people’s lives.</p>
         
        </div>
-       <div className="team gap-8 flex flex-col">
-        <div className="members px-16 gap-16 flex flex-col">
+       <div className="members gap-8 flex flex-col lg:flex-row lg:gap-4">
         <AboutMember 
             name={"Eyad Abdelfattah"}  
             role={"Backend Developer"}
-            about={"Eyad, a Muslim, an  Egyptian, an Arab, with a modest knowledge of backend programming and databases, deeply passionate about data and information and  Fond of statistics, especially those that can be used to solve problems. Add this description as the first cause on three other causes. First, going through one of the darkest experiences of my life, if not the darkest, with someone very close to me because of pornography. Second, a need for personal project to build my professional portfolio. Lastly, a profound desire to leave a positive impact that can benefit people, both during my lifetime and after. Those 4 reasons drove me to contribute to the creation of this humble platform. By God's grace."}
+            about={"I'm a Muslim, an Egyptian, an Arab, with a growing knowledge of backend programming and a passion for data and statistics. My contribution to this platform is driven by four key reasons: a personal struggle with someone close due to pornography, the need to build my professional portfolio, and a deep desire to create a positive, lasting impact. These experiences have shaped my motivation to help build this platform, by God's grace."}
             linkedin={"https://www.linkedin.com/in/eyad-fattah/"} 
             gmail={"mailto:eyadabdelfattah4@gmail.com"} 
             github={"https://github.com/eyadfattah23"} 
@@ -42,6 +41,6 @@ Melius has grown into more than just a technical project. It’s a tool built wi
         </div>
        </div>
        
-    </div>
+  
 }
 export default About
--- frontend/src/components/landing_page/about_member.jsx
+++ frontend/src/components/landing_page/about_member.jsx
@@ -1,36 +1,30 @@
 import Icon from "../../assets/icons/icon";
 
-function AboutMember({ name, role, about, linkedin, gmail, github, picture, reverse }) {
+function AboutMember({ name, role, about, linkedin, gmail, github, picture }) {
   return (
     <div
-      className={`team_member flex flex-col items-center gap-8 ${reverse ? "lg:flex-row-reverse" : "lg:flex-row"}`}
+      className={`team_member flex flex-col items-center`}
     >
-      <div
-        className="member_card bg-cover bg-center w-full h-80 md:h-auto lg:w-1/2"
-        style={{ backgroundImage: `url(${picture})` }}
-      >
-        <div className="information bg-yellow-300 p-4 w-3/4">
-          <span className="name font-bold">{name}</span>
-          <span className="role text-gray-700">{role}</span>
-        </div>
+      <div className="img" style={{ backgroundImage: `url(${picture})` }}>  
       </div>
-      <div className="description flex flex-col gap-4 w-full lg:w-1/2">
-        <p>{about}</p>
-        <div className="social_media flex flex-col gap-4">
-          <p>Follow me for more updates</p>
-          <div className="social_links flex gap-8">
+      <div className="information flex flex-col items-center gap-2">
+          <h3 className="name font-bold">{name}</h3>
+          <p className="role text-gray-700">{role}</p>
+          <p>{about}</p>      
+      </div>
+      <div className="social_media flex  justify-evenly">
             <a href={github} target="_blank" rel="noopener noreferrer">
-              <Icon name="github" size={24} color="icon_secondary" />
+              <Icon name="github" size={24} />
             </a>
             <a href={gmail} target="_blank" rel="noopener noreferrer">
-              <Icon name="email" size={24} color="icon_secondary" />
+              <Icon name="email" size={24}  />
             </a>
             <a href={linkedin} target="_blank" rel="noopener noreferrer">
-              <Icon name="linkedin" size={24} color="icon_secondary" />
+              <Icon name="linkedin" size={24}  />
             </a>
           </div>
-        </div>
-      </div>
+        
+      
     </div>
   );
 }
